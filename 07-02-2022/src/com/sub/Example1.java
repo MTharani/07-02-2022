4819fb6170fc0a3545cba7144dcc1b95ff943a38
Thara <Thara@statsfe2.update.microsoft.com.akadns.net>
2022-02-07 15:21:43 +0530

    first commit

---
+++ 07-02-2022/src/com/sub/Example1.java
@@ -0,0 +1,14 @@
+package com.sub;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int a, b, c;
+		a = 40;
+        b = 20;
+        c= a-b;
+        System.out.println("a-b:"+c);
+
+	}
+
+}
